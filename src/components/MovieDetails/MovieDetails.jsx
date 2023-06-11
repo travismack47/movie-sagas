24bb15bd16342bcf4fe4f1332ad404d4e6b6557a
travismack47 <travismack47@protonmail.com>
2023-06-11 10:59:27 -0500

    added styling to the home button on the details page

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,36 +1,77 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from 'react-router-dom';
+import { Typography, Button, makeStyles } from '@material-ui/core';
 
-function Details() {
+const useStyles = makeStyles(theme => ({
+  detailsContainer: {
+    textAlign: 'center',
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+  },
+  description: {
+    marginBottom: theme.spacing(2),
+  },
+  homeButton: {
+    fontSize: '1.5rem',
+    marginTop: theme.spacing(4),
+    padding: theme.spacing(2),
+    borderRadius: '50%',
+    textTransform: 'uppercase',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    color: 'white',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    '&:hover': {
+      background: 'linear-gradient(45deg, #FF8E53 30%, #FE6B8B 90%)',
+    },
+  },
+}));
+
+
+function MovieDetails() {
+    const classes = useStyles();
     const history = useHistory();
     const { id } = useParams();
     const dispatch = useDispatch();
     const movieDetails = useSelector(state => state.movieDetails);
 
+  
     useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: { id } });
+      dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: { id } });
     }, [dispatch, id]);
+  
     // Click handler to bring user to homepage from details page
     const handleClick = () => {
-        history.push('/');
+      history.push('/');
     };
-
+  
     return (
-        <>
-            <main>
-                <h1>Movie Details</h1>
-                <section>
-                    <h2>Title: {movieDetails.title}</h2>
-                    <img src={movieDetails.poster} alt={movieDetails.title} />
-                    <p>Description: {movieDetails.description}</p>
-                    <p>Genre: {movieDetails.genres}</p>
-                </section>
-            </main>
-            <button onClick={handleClick}>Home</button>
-        </>
+      <main>
+        <div className={classes.detailsContainer}>
+          <Typography variant="h1" className={classes.title}>
+            Movie Details
+          </Typography>
+          <section>
+            <Typography variant="h2">Title: {movieDetails.title}</Typography>
+            <img src={movieDetails.poster} alt={movieDetails.title} />
+            <Typography variant="body1" className={classes.description}>
+              Description: {movieDetails.description}
+            </Typography>
+            <Typography variant="body1">Genre: {movieDetails.genres}</Typography>
+          </section>
+        </div>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.homeButton}
+          onClick={handleClick}
+        >
+          Home
+        </Button>
+      </main>
     );
-}
+  }
 
-export default Details;
+  export default MovieDetails;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 
+const useStyles = makeStyles(theme => ({
+    heading: {
+        marginBottom: theme.spacing(2), // Adjust the spacing as needed
+    },
+}));
+
 function MovieList() {
+    const classes = useStyles();
     const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
@@ -15,12 +22,14 @@ function MovieList() {
 
     return (
         <main>
-            <Typography variant='h1'>Movies Saga!</Typography>
-            <Typography variant="h3">List of Films</Typography>
+            <Typography variant='h2'>Movies Saga!</Typography>
+            <Typography variant="h3" className={classes.heading}>
+                List of Films
+            </Typography>
             <Grid container spacing={2}>
                 {movies.map(movie => (
                     <Grid item xs={12} sm={6} md={4} key={movie.id}>
-                        <Typography variant="h3">{movie.title}</Typography>
+                        <Typography variant="h4">{movie.title}</Typography>
                         <img
                             src={movie.poster}
                             alt={movie.title}
