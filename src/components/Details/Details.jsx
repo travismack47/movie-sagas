43b4931a9b74e7f2a7dcb44d894ca7f5d388d524
travismack47 <travismack47@protonmail.com>
2023-06-10 21:16:50 -0500

    added Home button on details page to bring user to homepage

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,29 +1,36 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function Details() {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const movieDetails = useSelector(state => state.movieDetails);
+    const history = useHistory();
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const movieDetails = useSelector(state => state.movieDetails);
 
-  useEffect(() => {
-    dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: { id } });
-  }, [dispatch, id]);
+    useEffect(() => {
+        dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: { id } });
+    }, [dispatch, id]);
 
-  return (
-    <>
-    <main>
-      <h1>Movie Details</h1>
-      <section>
-        <h2>Title: {movieDetails.title}</h2>
-        <img src={movieDetails.poster} alt={movieDetails.title} />
-        <p>Description: {movieDetails.description}</p>
-        <p>Genre: {movieDetails.genres}</p>
-      </section>
-    </main>
-    </>
-  );
+    const handleClick = () => {
+        history.push('/');
+    };
+
+    return (
+        <>
+            <main>
+                <h1>Movie Details</h1>
+                <section>
+                    <h2>Title: {movieDetails.title}</h2>
+                    <img src={movieDetails.poster} alt={movieDetails.title} />
+                    <p>Description: {movieDetails.description}</p>
+                    <p>Genre: {movieDetails.genres}</p>
+                </section>
+            </main>
+            <button onClick={handleClick}>Home</button>
+        </>
+    );
 }
 
 export default Details;
